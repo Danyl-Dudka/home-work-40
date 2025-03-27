c345e2fdc4780e679cac2c9b5b0c27edc240cdcc
Danyl-Dudka <danyad.stokes@gmail.com>
2025-03-27 02:09:05 -0400

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { useEffect } from 'react'
 import ListButton from '../ListButton/ListButton'
 import AddUserButton from '../AddUserButton/AddUserButton'
+import {BrowserRouter, Routes, Route, Link} from "react-router";
 function App() {
   const [users, setUsers] = useState(0);
   const [isListOpen, setIsListOpen] = useState(false);
@@ -26,6 +27,12 @@ function App() {
   
   return (
     <>
+    <BrowserRouter>
+    <Link to="/list"><ListButton users={users} setUsers={setUsers} isOpen={isListOpen} setIsOpen={openList}/></Link>
+    <Link to="/add"><AddUserButton users={users} setUsers={setUsers} isOpen={isAddOpen} setIsOpen={openAdd} openList={openList}/></Link>
+    <Routes>
+    <Route path="/list" element={<ListButton users={users} setUsers={setUsers} isOpen={isListOpen} setIsOpen={openList}/>}/>
+    <Route path="/add" element={<AddUserButton users={users} setUsers={setUsers} isOpen={isAddOpen} setIsOpen={openAdd} openList={openList}/>}/>
     <div className='button-container'>
     <ListButton users={users} setUsers={setUsers} isOpen={isListOpen} setIsOpen={openList}/>
     <AddUserButton users={users} setUsers={setUsers} isOpen={isAddOpen} setIsOpen={openAdd} openList={openList}/>
